7e740bc0aa894f217325a003e10b254a93291d53
zunstar <shimkijun@gmail.com>
2023-11-12 21:15:56 +0900

    useAsync 커스텀 Hook 만들어서 사용하기

---
+++ src/User.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import React from "react";
+import useAsync from "./useAsync";
+
+async function getUsers(id){
+    const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    return response.data;
+};
+
+function User( {id}){
+    const [state] = useAsync(()=>getUsers(id), [id]);
+    const {loading, data:user, error} = state;
+    
+    if(loading) return <div>로딩중..</div>;
+    if(error) return <div>에러가 발생했습니다.</div>;
+    if(!user) return null;
+
+  return(
+    <div>
+        <h2>{user.username}</h2>
+        <p>
+            <b>Email:</b> {user.email}
+        </p>
+    </div>
+  )
+}
+
+export default User;
